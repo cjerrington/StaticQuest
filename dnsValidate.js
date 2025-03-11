fc5c9a42c7ca9cbb2cb4aff436c029ccda459112
Clayton Errington <me@claytonerrington.com>
2025-03-10 22:53:03 -0500

    update dnsValidate.js

--- dnsValidate.js
+++ dnsValidate.js
@@ -1,4 +1,10 @@
 const dns = require('dns');
+const { readFileSync } = require('fs');
 
-dns.resolveTxt('claytonerrington.com', (err,
-    addresses) => console.log('TXT records: %j', addresses));
+const data = readFileSync('./_site/assets/members.json');
+sites = JSON.parse(data);
+
+sites.forEach(site => {
+    dns.resolve4(site.host, (err,
+        addresses) => console.log('A records for '+site.host+': %j', addresses));
+});
